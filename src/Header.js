9c4fa32b2a81df44829025c7c92c31a1722d2f59
Mouhamed <golf452013@gmail.com>
2022-12-10 09:02:54 +0000

    added page 4

--- src/Header.js
+++ src/Header.js
@@ -14,6 +14,7 @@ function Header (){
                     <li><Link as={Link} to="/page1"  > Page 1</Link></li>
                     <li><Link as={Link} to="/page2"  > Page 2</Link></li>
                     <li><Link as={Link} to="/page3"  > Page 3</Link></li>
+                    <li><Link as={Link} to="/page4"  > Page 4</Link></li>
                 </ul>
             </div>
         </div>  
