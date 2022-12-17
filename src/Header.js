9b7d42b11099dd62fa37c9672bc3f1a46ccdc7eb
Mouhamed <golf452013@gmail.com>
2022-12-17 16:50:58 +0000

    improved a lot

--- src/Header.js
+++ src/Header.js
@@ -7,12 +7,13 @@ function Header (){
             <div className="a-header">
                 
             <input type="checkbox" name="main-nav" id="main-nav" className="burger-check" />
+
                 <label htmlFor="main-nav" className="burger menu"><span></span></label>
-                <ul>
-                    <li><Link as={Link} to="/home" > Home </Link></li>
-                    <li><Link as={Link} to="/page1" > Page 1 </Link></li>
-                    <li><Link as={Link} to="/page2" > Page 2 </Link></li>
-                    <li><Link as={Link} to="/page3" > Page 3 </Link></li>
+                <ul className='ulblock'>
+                    <li><Link as={Link} to="/home" > <span> Home </span> </Link></li>
+                    <li><Link as={Link} to="/page1" > <span> Beach </span> </Link></li>
+                    <li><Link as={Link} to="/page2" > <span> Bedroom </span> </Link></li>
+                    <li><Link as={Link} to="/page3" > <span> Street </span> </Link></li>
                 </ul>
                 
             </div>
